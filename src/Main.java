d430d613ef0863ff4c7d30daef005300f80b69ae
Gregory <parfeniuk33@gmail.com>
2022-10-13 19:55:59 +0300

    Hope the final version

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args){
+        final int[] arr = new int[1000];
+        for (int i = 0; i < arr.length; ++i){
+            arr[i] = (int) (Math.random() * arr.length);
+        }
+        final int[] arrClone = arr.clone();
+        QuickSort.quickSort(arr);
+        Arrays.sort(arrClone);
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.equals(arr, arrClone));
+
+    }
 }
--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class QuickSort {
+public class QuickSort {
+
+    public static void quickSort(final int[] arr){
+        if (arr == null) return;
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private static void quickSort(final int[] arr, final int lowIndex, final int highIndex){
+        if (lowIndex < highIndex){
+            final int pivotIndex = partition(arr, lowIndex, highIndex);
+            quickSort(arr, lowIndex, pivotIndex);
+            quickSort(arr, pivotIndex + 1, highIndex);
+        }
+    }
+
+    private static int partition(final int[] arr, final int lowIndex, final int highIndex){
+        final int pivot = arr[lowIndex];
+        int leftPtr = lowIndex, rightPtr = highIndex;
+        while (true){
+            while(arr[leftPtr] < pivot) ++leftPtr;
+            while(arr[rightPtr] > pivot) -- rightPtr;
+            if (leftPtr >= rightPtr){
+                return rightPtr;
+            }
+            swap(arr, leftPtr, rightPtr);
+            ++leftPtr;
+            --rightPtr;
+        }
+    }
+
+
+
+    private static void swap(final int[] arr, final int index1, final int index2){
+        final int buf = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = buf;
+    }
 }
